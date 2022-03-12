fa59c389b067dd6c132cfca8cd892aecd0422aae
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-12 19:06:34 +0100

    Created sessions reducer and its test

---
+++ src/redux/reducers/sessionsReducer.ts
@@ -0,0 +1,19 @@
+import { actionTypes } from "../actions/actionTypes";
+import { SessionShape } from "../../components/Session/Session";
+
+const sessionsReducer = (
+  sessions: SessionShape[],
+  action: { type: string; sessions: SessionShape[] }
+) => {
+  let newSessionsList;
+
+  if (action.type === actionTypes.loadSessionsList) {
+    newSessionsList = [...action.sessions];
+  } else {
+    newSessionsList = [...sessions];
+  }
+
+  return newSessionsList;
+};
+
+export default sessionsReducer;
