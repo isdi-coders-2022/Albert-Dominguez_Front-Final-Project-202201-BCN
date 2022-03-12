03182e3b095eaba4930d9a824a81479081f4d786
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-12 19:27:13 +0100

    Created sessions thunk

---
+++ src/redux/thunks/sessionsThunk.ts
@@ -0,0 +1,12 @@
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { loadSessionsAction } from "../actions/actionsCreator";
+
+export const loadSessionsListThunk = async (
+  dispatch: ThunkDispatch<void, unknown, AnyAction>
+) => {
+  const response = await fetch(`${process.env.REACT_APP_API}sessions/`);
+
+  const sessionsListResponse = await response.json();
+  dispatch(loadSessionsAction(sessionsListResponse));
+};
