6cb8a46ea449211867b2f886739e89c36a1cd09f
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-13 23:43:32 +0100

    Created PatientPage, patient component, actions & thunk

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Navigation from "./components/Navigation/Navigation";
 import CalendarPage from "./pages/CalendarPage/CalendarPage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PatientsPage from "./pages/PatientsPage/PatientsPage";
 import SessionsPage from "./pages/SessionsPage/SessionsPage";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Routes>
         <Route>
+          <Route path="/patients" element={<PatientsPage />} />
           <Route path="/sessions" element={<SessionsPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
           <Route path="*" element={<NotFoundPage />} />
---
+++ src/redux/thunks/usersThunk.ts
@@ -0,0 +1,11 @@
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
+import { loadPatientsAction } from "../actions/actionsCreator";
+
+export const loadPatientssListThunk = async (
+  dispatch: ThunkDispatch<void, unknown, AnyAction>
+) => {
+  const response = await fetch(`${process.env.REACT_APP_API}users/`);
+  const patientsListResponse = await response.json();
+  dispatch(loadPatientsAction(patientsListResponse));
+};
