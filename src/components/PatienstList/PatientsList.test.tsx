eb7d187be40e12bd748cb19f1c84fb4c2d0c26a2
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-15 15:01:06 +0100

    Renamed PatientList to PatientsList

--- src/components/PatienstList/PatientsList.test.tsx
+++ src/components/PatienstList/PatientsList.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import PatientsList from "./PatientList";
+import PatientsList from "./PatientsList";
 
 describe("Given a PatientList component", () => {
   describe("When it's instantiated", () => {
