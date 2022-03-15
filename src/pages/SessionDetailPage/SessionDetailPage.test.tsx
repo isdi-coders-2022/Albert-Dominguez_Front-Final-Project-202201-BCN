58bd845b916657287bd813ec2bb96bc028413551
albertdc1984 <92040589+albertdc1984@users.noreply.github.com>
2022-03-15 12:32:23 +0100

    Tried to mock useParams for sessionStailPage test but couldn't

--- src/pages/SessionDetailPage/SessionDetailPage.test.tsx
+++ src/pages/SessionDetailPage/SessionDetailPage.test.tsx
@@ -3,16 +3,18 @@ import { Provider } from "react-redux";
 import { store } from "../../redux/store";
 import SessionDetailPage from "./SessionDetailPage";
 
-describe("Given a SessionsPage component", () => {
-  describe("When it's instantiated", () => {
-    test("Then it should render a list of session cards", () => {
+describe("Given a SessionDetailPage component", () => {
+  describe("When it's instantiated without session params", () => {
+    test("Then it should render a not found page", () => {
       render(
         <Provider store={store}>
           <SessionDetailPage />
         </Provider>
       );
 
-      const paragraph = screen.getByRole("heading", { name: "MY SESSIONS" });
+      const paragraph = screen.getByRole("heading", {
+        name: "SORRY, PAGE NOT FOUND",
+      });
 
       expect(paragraph).toBeInTheDocument();
     });
